94fd9b163f8fa7d43f025325922ce84cd52dad1a
Fábio Petkowicz <fopetkowicz@gmail.com>
2021-09-12 00:15:33 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -2,7 +2,6 @@ package com.devsuperior.dsvendas.controllers;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,7 @@ import com.devsuperior.dsvendas.service.SellerService;
 
 @RestController
 @RequestMapping(value = "/sellers")
-public class SellerControler {
+public class SellerController {
 	
 	@Autowired
 	private SellerService service;
